b69e8eec3290c4216964a961353040805f5c5fba
Alex <alexandre.chastaing@non.se.com>
2024-06-12 10:59:35 +0200

    task manager: end course

--- TaskManager/app/src/main/java/com/example/taskmanager/MainActivity.kt
+++ TaskManager/app/src/main/java/com/example/taskmanager/MainActivity.kt
@@ -4,13 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.taskmanager.ui.theme.TaskManagerTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,9 +30,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             TaskManagerTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Page()
-                }
+                Page()
             }
         }
     }
@@ -29,15 +38,29 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Page() {
-    Column{
-        Image(),
-        
-    }
+        Column (
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ){
+            val image = painterResource(id = R.drawable.ic_task_completed)
+            Image(painter = image, contentDescription = "completed task")
+            Text(
+                text = stringResource(R.string.all_tasks_complete),
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier
+                    .padding(top = 24.dp, bottom = 8.dp)
+            )
+            Text(
+                text = stringResource(R.string.nice_works),
+                fontSize = 16.sp
+            )
+        }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun PagePreview() {
     TaskManagerTheme {
         Page()
     }
